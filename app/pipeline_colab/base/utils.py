9f72a08abec2e1fd064378019b8295ecee56f45a
devoure <disguisedsandwich@gmail.com>
2024-04-23 12:38:49 +0300

    Tested specific run search

--- app/pipeline_colab/base/utils.py
+++ app/pipeline_colab/base/utils.py
@@ -29,3 +29,34 @@ def get_data():
     context = {"agents": agents}
 
     return context
+
+
+
+
+def get_predictions(run_id):
+    mlflow.set_tracking_uri('http://192.168.1.201:5000')
+
+    experiment_id = 'Housing'
+    run = mlflow.search_runs(experiment_names=[experiment_id],
+                              filter_string="attributes.run_id = '{}'".format(run_id))
+    for row in run.iterrows():
+        print(row[1]["tags.mlflow.log-model.history"])
+
+
+
+def get_run():
+    mlflow.set_tracking_uri('http://192.168.1.201:5000')
+
+    experiment_id = 'Housing'
+    runs = mlflow.search_runs(experiment_names=[experiment_id],
+                              filter_string="attributes.status = 'FINISHED'")
+
+    for run in runs.iterrows():
+        agent = {}
+        agent.update({"run_id": run[1]['run_id']}) 
+
+    print(runs)
+    print("SSS>>", agent)
+    get_predictions(agent["run_id"])
+
+get_run()
